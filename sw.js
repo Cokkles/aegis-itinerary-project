ab2462a04f543c63352a2fa72476fe1d69ef3390
BNeuro <teasleybrian@gmail.com>
2026-08-19 22:02:14 -0400

    Bump AEGIS cache for dashboard renderer update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='aegis-dashboard-v2.1.0';
+const CACHE_NAME='aegis-dashboard-v2.2.0';
 const STATIC=['./manifest.json','./icon-192.png','./icon-512.png','./favicon.ico'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
